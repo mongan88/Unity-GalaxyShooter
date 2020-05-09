0bccd348b40164b6a5544e549e13e21bc51ed68a
blair.mongan <blair.mongan@gig.com>
2020-05-09 16:26:02 +0200

    Initial solution with movement using WASD implemented.

---
+++ Galaxy Shooter/Assets/Configuration/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Configuration
+{
+    public sealed class Constants
+    {
+        public const string HorizontalInputKey = "Horizontal";
+        public const string VerticalInputKey = "Vertical";
+    }
+}
---
+++ Galaxy Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Configuration;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] // Allows value to be modified in the designer
+    private float _speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis(Constants.HorizontalInputKey);
+        var verticalInput = Input.GetAxis(Constants.VerticalInputKey);
+
+        var direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+    }
+}
